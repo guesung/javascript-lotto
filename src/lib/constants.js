93a1f70846a8c535b5c6ac876571bc5b127d3b7b
guesung <gueit214@naver.com>
2025-02-22 16:21:53 +0900

    refactor: 개수 및 숫자 상수화

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,6 +1,7 @@
 export const MIN_LOTTO_NUMBER = 1;
 export const MAX_LOTTO_NUMBER = 45;
 export const LOTTO_LENGTH = 6;
+export const BONUS_NUMBER_COUNT = 6;
 export const LOTTO_PRICE = 1_000;
 export const SEPERATOR = ',';
 export const NO_WINNING = '당첨 없음';
@@ -11,15 +12,15 @@ const ERROR_MESSAGE_DEFAULT = '[ERROR]';
 export const ERROR_MESSAGES = {
   purchaseAmount: {
     positiveInteger: `${ERROR_MESSAGE_DEFAULT} 양의 정수를 입력해주세요.`,
-    thousandUnit: `${ERROR_MESSAGE_DEFAULT} 1,000단위로 입력해주세요.`,
+    thousandUnit: `${ERROR_MESSAGE_DEFAULT} ${LOTTO_PRICE.toLocaleString()}단위로 입력해주세요.`,
   },
   winNumber: {
     unique: `${ERROR_MESSAGE_DEFAULT} 중복되지 않은 숫자로 입력해주세요.`,
-    range: `${ERROR_MESSAGE_DEFAULT} 6개의 ${MIN_LOTTO_NUMBER}~${MAX_LOTTO_NUMBER} 사이의 정수로 입력해주세요.`,
+    range: `${ERROR_MESSAGE_DEFAULT} ${LOTTO_LENGTH}개의 ${MIN_LOTTO_NUMBER}~${MAX_LOTTO_NUMBER} 사이의 정수로 입력해주세요.`,
   },
   bonusNumber: {
     unique: `${ERROR_MESSAGE_DEFAULT} 당첨 번호와 중복되지 않게 입력해주세요.`,
-    range: `${ERROR_MESSAGE_DEFAULT} 1개의 ${MIN_LOTTO_NUMBER}~${MAX_LOTTO_NUMBER} 사이의 정수로 입력해주세요.`,
+    range: `${ERROR_MESSAGE_DEFAULT} ${BONUS_NUMBER_COUNT}개의 ${MIN_LOTTO_NUMBER}~${MAX_LOTTO_NUMBER} 사이의 정수로 입력해주세요.`,
   },
   retry: {
     yesOrNo: `${ERROR_MESSAGE_DEFAULT} ${YES} 또는 ${NO}을 입력해주세요.`,
