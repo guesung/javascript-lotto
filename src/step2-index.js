0bf46110575f0a295eaa092cc6df6969f4f10931
guesung <gueit214@naver.com>
2025-02-24 15:47:49 +0900

    refactor: innerHMTL을 활용해 리팩토링

--- src/step2-index.js
+++ src/step2-index.js
@@ -11,56 +11,32 @@ const handleSubmit = (e) => {
   const purchasedLottos = LottoShop.createLotto(purchaseCount);
 
   const purchaseLottoCountBox = document.createElement('div');
-  purchaseLottoCountBox.className = 'winning-count-box';
-  purchaseLottoCountBox.innerText = `총 ${purchaseCount}개를 구매하였습니다.`;
 
-  container.appendChild(purchaseLottoCountBox);
-
-  const purchaseLottosBox = document.createElement('div');
-  purchasedLottos.forEach((purchasedLotto) => {
-    const purchasedLottoBox = document.createElement('div');
-    purchasedLottoBox.innerText = purchasedLotto.numbers.join(', ');
-    purchaseLottosBox.appendChild(purchasedLottoBox);
-  });
-
-  container.appendChild(purchaseLottosBox);
-
-  const winningBox = document.createElement('div');
-  const winningInfo = document.createElement('p');
-  winningInfo.innerText = '지난 주 당첨번호 6개와 보너스 번호 1개를 입력해주세요.';
-
-  winningBox.appendChild(winningInfo);
-
-  const winningNumbersBox = document.createElement('div');
-  const winningNumbersInfo = document.createElement('span');
-  winningNumbersInfo.innerText = '당첨 번호';
-
-  const winningNumbersInputBox = document.createElement('div');
-  for (let i = 0; i < 6; i += 1) {
-    const winningNumbersInput = document.createElement('input');
-    winningNumbersInputBox.appendChild(winningNumbersInput);
-  }
-
-  winningNumbersBox.appendChild(winningNumbersInfo);
-  winningNumbersBox.appendChild(winningNumbersInputBox);
-
-  container.appendChild(winningBox);
-  container.appendChild(winningNumbersBox);
-
-  const bonusNumberBox = document.createElement('div');
-  const bonusNumberInfo = document.createElement('span');
-  bonusNumberInfo.innerText = '보너스 번호';
-
-  const bonusNumberInputBox = document.createElement('input');
-
-  bonusNumberBox.appendChild(bonusNumberInfo);
-  bonusNumberBox.appendChild(bonusNumberInputBox);
-
-  container.appendChild(bonusNumberBox);
-
-  const resultButton = document.createElement('button');
-  resultButton.innerHTML = '결과 확인하기';
-  container.appendChild(resultButton);
+  container.innerHTML = `
+    <section>
+      <p>총 ${purchaseCount}개를 구매하였습니다.</p>
+      <ul>
+        ${purchasedLottos.map((purchasedLotto) => `<li>${purchasedLotto.numbers.join(', ')}</li>`).join('')}
+      </ul>
+    </section>
+    <form>
+      <p>지난 주 당첨번호 6개와 보너스 번호 1개를 입력해주세요.</p>
+      <div>
+        <span>당첨 번호</span>
+        <input/>
+        <input/>
+        <input/>
+        <input/>
+        <input/>
+        <input/>
+      </div>
+      <div>
+        <span>보너스 번호</span>
+        <input/>
+      </div>
+      <button>결과 확인하기</button>
+    </form>
+  `;
 };
 
 const form = document.querySelector('form');
