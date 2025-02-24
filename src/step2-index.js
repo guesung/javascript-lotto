5634dd67df50ee6df4322cb6af98a64b2d92a66a
guesung <gueit214@naver.com>
2025-02-24 16:21:32 +0900

    feat: 당첨 결과 화면 구현

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,4 +1,7 @@
+import LottoCompany from './domain/LottoCompany.js';
 import LottoShop from './domain/LottoShop.js';
+import { LOTTO_RANK_INFO } from './lib/constants.js';
+import { calculateMatchCount, calculateProfitRate } from './lib/utils.js';
 
 const container = document.getElementById('container');
 
@@ -10,34 +13,77 @@ const handleSubmit = (e) => {
   const purchaseCount = LottoShop.calculateLottoCount(purchaseAmount);
   const purchasedLottos = LottoShop.createLotto(purchaseCount);
 
-  const purchaseLottoCountBox = document.createElement('div');
-
-  container.innerHTML = `
+  const lottoInfoContainer = document.createElement('div');
+  lottoInfoContainer.innerHTML = `
     <section>
       <p>총 ${purchaseCount}개를 구매하였습니다.</p>
       <ul>
         ${purchasedLottos.map((purchasedLotto) => `<li>${purchasedLotto.numbers.join(', ')}</li>`).join('')}
       </ul>
     </section>
-    <form>
+    <form id="result">
       <p>지난 주 당첨번호 6개와 보너스 번호 1개를 입력해주세요.</p>
       <div>
         <span>당첨 번호</span>
-        <input/>
-        <input/>
-        <input/>
-        <input/>
-        <input/>
-        <input/>
+        <input class='winning-number' value='1' />
+        <input class='winning-number' value='2' />
+        <input class='winning-number' value='3' />
+        <input class='winning-number' value='4' />
+        <input class='winning-number' value='5' />
+        <input class='winning-number' value='6' />
       </div>
       <div>
         <span>보너스 번호</span>
-        <input/>
+        <input class='bonus-number' value='7' />
       </div>
-      <button>결과 확인하기</button>
+      <button id='showResult'>결과 확인하기</button>
     </form>
   `;
+
+  container.appendChild(lottoInfoContainer);
+
+  const handleResultButtonClick = (event) => {
+    event.preventDefault();
+
+    const winningNumberInputs = document.querySelectorAll('.winning-number');
+    const winningNumbers = [...winningNumberInputs].map((winningNumberInput) => Number(winningNumberInput.value));
+    const bonusNumber = Number(document.querySelector('.bonus-number').value);
+
+    const lottoCompany = new LottoCompany(winningNumbers, bonusNumber);
+    const lottoRanks = lottoCompany.calculateLottoRanks(purchasedLottos);
+
+    const totalPrize = lottoCompany.calculateTotalProfit(lottoRanks);
+    const profitRate = calculateProfitRate(totalPrize, purchaseAmount);
+
+    const winningStaticsModal = document.createElement('div');
+    winningStaticsModal.innerHTML = `
+      <h2>🏆 당첨 통계 🏆</h2>
+      <table>
+        <tr>
+          <td>일치 갯수</td>
+          <td>당첨금</td>
+          <td>당첨 갯수</td>
+        </tr>
+        ${[...Object.keys(LOTTO_RANK_INFO)].reverse().map((lottoRank) => {
+          const lottoRankInfo = LOTTO_RANK_INFO[lottoRank];
+          const rankCount = calculateMatchCount(lottoRanks, lottoRank);
+
+          return `
+            <tr>
+              <td>${lottoRankInfo.winNumber}개</td>
+              <td>${lottoRankInfo.prize.toLocaleString()}</td>
+              <td>${rankCount}개</td>
+            </tr>
+          `;
+        })}
+      </table>
+      <p>당신의 총 수익률은 ${profitRate}%입니다.</p>
+      <button>다시 시작하기</button>
+    `;
+    container.appendChild(winningStaticsModal);
+  };
+
+  document.getElementById('result').addEventListener('submit', handleResultButtonClick);
 };
 
-const form = document.querySelector('form');
-form.addEventListener('submit', handleSubmit);
+document.querySelector('form').addEventListener('submit', handleSubmit);
