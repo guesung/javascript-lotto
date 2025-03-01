596e76e0dfa4d3238e540f29f7dc872bf4f03ca9
guesung <gueit214@naver.com>
2025-03-01 16:44:10 +0900

    test: 각 예외 사항에 맞는 테스트 케이스

--- __tests__/Application.js
+++ __tests__/Application.js
@@ -1,4 +1,5 @@
 import App from '../src/App.js';
+import { ERROR_MESSAGES } from '../src/lib/constants.js';
 import * as utils from '../src/lib/utils.js';
 
 const mockReadLineAsync = (mockValues) => {
@@ -70,6 +71,100 @@ describe('Application', () => {
 
       [[1, 2, 3, 4, 5, 6]].forEach((expectedConsoleLogMessage) => {
         expect(consoleLogSpy).toHaveBeenCalledWith(expectedConsoleLogMessage);
+      });
+    });
+  });
+
+  describe('예외 사항 처리', () => {
+    describe('구입금액', () => {
+      test('구입 금액은 양의 정수여야한다.', async () => {
+        mockReadLineAsync(['0', '1000', '1,2,3,4,5,6', '7', 'n']);
+        mockGenerateUniqueNumbers([[3, 2, 5, 4, 1, 6]]);
+
+        const app = new App();
+        await app.run();
+
+        [ERROR_MESSAGES.purchaseAmount.positiveInteger].forEach((expectedConsoleLogMessage) => {
+          expect(consoleLogSpy).toHaveBeenCalledWith(expectedConsoleLogMessage);
+        });
+      });
+      test('구입 금액은 1000으로 나누어 떨어져야한다.', async () => {
+        mockReadLineAsync(['10', '1000', '1,2,3,4,5,6', '7', 'n']);
+        mockGenerateUniqueNumbers([[3, 2, 5, 4, 1, 6]]);
+
+        const app = new App();
+        await app.run();
+
+        [ERROR_MESSAGES.purchaseAmount.thousandUnit].forEach((expectedConsoleLogMessage) => {
+          expect(consoleLogSpy).toHaveBeenCalledWith(expectedConsoleLogMessage);
+        });
+      });
+    });
+
+    describe('당첨 번호', () => {
+      test('당첨 번호는 중복되지 않은 숫자여야한다.', async () => {
+        mockReadLineAsync(['1000', '1,2,3,4,5,5', '1,2,3,4,5,6', '7', 'n']);
+        mockGenerateUniqueNumbers([[3, 2, 5, 4, 1, 6]]);
+
+        const app = new App();
+        await app.run();
+
+        [ERROR_MESSAGES.winNumber.unique].forEach((expectedConsoleLogMessage) => {
+          expect(consoleLogSpy).toHaveBeenCalledWith(expectedConsoleLogMessage);
+        });
+      });
+      test.each(['1,2,3,4,5,6,7', '1,2,3,4,5', '1,2,3,4,5,46', '0,1,2,3,4,5'])(
+        '당첨 번호은 6개의 1-45 사이의 정수여야한다.',
+        async (value) => {
+          mockReadLineAsync(['1000', value, '1,2,3,4,5,6', '7', 'n']);
+          mockGenerateUniqueNumbers([[3, 2, 5, 4, 1, 6]]);
+
+          const app = new App();
+          await app.run();
+
+          [ERROR_MESSAGES.winNumber.range].forEach((expectedConsoleLogMessage) => {
+            expect(consoleLogSpy).toHaveBeenCalledWith(expectedConsoleLogMessage);
+          });
+        },
+      );
+    });
+
+    describe('보너스 번호', () => {
+      test('보너스 번호는 당첨 번호와 중복되면 안된다.', async () => {
+        mockReadLineAsync(['1000', '1,2,3,4,5,6', '6', '7', 'n']);
+        mockGenerateUniqueNumbers([[3, 2, 5, 4, 1, 6]]);
+
+        const app = new App();
+        await app.run();
+
+        [ERROR_MESSAGES.bonusNumber.unique].forEach((expectedConsoleLogMessage) => {
+          expect(consoleLogSpy).toHaveBeenCalledWith(expectedConsoleLogMessage);
+        });
+      });
+      test.each(['0', '46', 'a', '1,2'])('보너스 번호는 1개의 1-45 사이의 정수여야한다.', async (value) => {
+        mockReadLineAsync(['1000', '1,2,3,4,5,6', value, '7', 'n']);
+        mockGenerateUniqueNumbers([[3, 2, 5, 4, 1, 6]]);
+
+        const app = new App();
+        await app.run();
+
+        [ERROR_MESSAGES.bonusNumber.range].forEach((expectedConsoleLogMessage) => {
+          expect(consoleLogSpy).toHaveBeenCalledWith(expectedConsoleLogMessage);
+        });
+      });
+    });
+
+    describe('재시작 여부', () => {
+      test.each(['Y', 'N', 'Of Course! Why Not?'])('재시작 여부는 y 또는 n이어야한다.', async (value) => {
+        mockReadLineAsync(['1000', '1,2,3,4,5,6', '7', value, 'n']);
+        mockGenerateUniqueNumbers([[3, 2, 5, 4, 1, 6]]);
+
+        const app = new App();
+        await app.run();
+
+        [ERROR_MESSAGES.retry.yesOrNo].forEach((expectedConsoleLogMessage) => {
+          expect(consoleLogSpy).toHaveBeenCalledWith(expectedConsoleLogMessage);
+        });
       });
     });
   });
