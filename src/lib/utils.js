a48e9d2c429c3404438ef651b8ba7fb302c85ba1
guesung <gueit214@naver.com>
2025-03-01 16:44:09 +0900

    refactor: 함수 선언문을 화살표 함수 표현식으로 통일

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,11 +1,7 @@
 import readline from 'readline';
 
-export function readLineAsync(query) {
-  return new Promise((resolve, reject) => {
-    if (arguments.length !== 1) {
-      reject(new Error('arguments must be 1'));
-    }
-
+export const readLineAsync = (query) =>
+  new Promise((resolve, reject) => {
     if (typeof query !== 'string') {
       reject(new Error('query must be string'));
     }
@@ -20,48 +16,36 @@ export function readLineAsync(query) {
       resolve(input);
     });
   });
-}
 
-export function generateRandomNumber(start, end) {
-  return Math.floor(Math.random() * (end + 1 - start)) + start;
-}
+export const generateRandomNumber = (start, end) => Math.floor(Math.random() * (end + 1 - start)) + start;
 
-export function generateUniqueRandomValue(array, { start, end }) {
+export const generateUniqueRandomValue = (array, { start, end }) => {
   const randomNumber = generateRandomNumber(start, end);
   if (array.includes(randomNumber)) return generateUniqueRandomValue(array, { start, end });
 
   return randomNumber;
-}
+};
 
-export function generateUniqueNumbers({ start, end }, length) {
-  return new Array(length).fill(null).reduce((prev) => {
+export const generateUniqueNumbers = ({ start, end }, length) =>
+  new Array(length).fill(null).reduce((prev) => {
     const uniqueRandomValue = generateUniqueRandomValue(prev, { start, end });
 
     return [...prev, uniqueRandomValue];
   }, []);
-}
 
-export function calculateProfitRate(profit, price) {
-  return Number(((profit / price) * 100).toFixed(1));
-}
+export const calculateProfitRate = (profit, price) => Number(((profit / price) * 100).toFixed(1));
 
-export function checkUniqueArray(array) {
-  return array.length === new Set(array).size;
-}
+export const checkUniqueArray = (array) => array.length === new Set(array).size;
 
-export function getIntersectCount(array1, array2) {
-  return array1.filter((value) => array2.includes(value)).length;
-}
+export const getIntersectCount = (array1, array2) => array1.filter((value) => array2.includes(value)).length;
 
-export async function retryUntilSuccess(callbackFunction, onError) {
+export const retryUntilSuccess = async (callbackFunction, onError) => {
   try {
     return await callbackFunction();
   } catch (error) {
     onError?.(error);
     return retryUntilSuccess(callbackFunction, onError);
   }
-}
+};
 
-export function calculateMatchCount(array, number) {
-  return array.filter((item) => item === number).length;
-}
+export const calculateMatchCount = (array, number) => array.filter((item) => item === number).length;
