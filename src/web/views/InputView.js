e92382543da3930844908861c6cbce381ca60949
guesung <gueit214@naver.com>
2025-03-01 16:44:10 +0900

    refactor: Namespace BEM으로 클래스명 통일

--- src/web/views/InputView.js
+++ src/web/views/InputView.js
@@ -1,18 +1,18 @@
 export default class InputView {
   static readPurchaseAmount() {
-    const purchaseAmountForm = document.getElementById('purchase');
+    const purchaseAmountForm = document.querySelector('.purchase__form');
     const purchaseAmountInput = purchaseAmountForm?.querySelector('input');
 
     return purchaseAmountInput?.value;
   }
 
   static readWinNumbers() {
-    const winningNumberInputs = document.querySelectorAll('.winning-number');
+    const winningNumberInputs = document.querySelectorAll('.winning__input--number');
 
     return [...winningNumberInputs].map((winningNumberInput) => Number(winningNumberInput.value));
   }
 
   static readBonusNumber() {
-    return Number(document.querySelector('.bonus-number').value);
+    return Number(document.querySelector('.winning__input--bonus-number').value);
   }
 }
