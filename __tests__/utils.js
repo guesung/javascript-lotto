cc2bef688b601fa9d4f87deb0390d9246b90bbd3
guesung <gueit214@naver.com>
2025-03-02 22:06:41 +0900

    docs: 테스트 설명글 수정

--- __tests__/utils.js
+++ __tests__/utils.js
@@ -23,7 +23,7 @@ describe('utils', () => {
 
   describe('generateUniqueNumbers', () => {
     context('시작 수와 종료 수가 주어져있을 때', () => {
-      it('중복되지 않는 숫자로 구성된 배열을 만든다.', () => {
+      it('중복되지 않는 숫자로 구성된 배열을 만들어 반환한다.', () => {
         const uniqueNumbers = generateUniqueNumbers({ start: 1, end: 6 }, 6);
 
         expect(uniqueNumbers).toEqual(expect.arrayContaining([1, 2, 3, 4, 5, 6]));
@@ -33,7 +33,7 @@ describe('utils', () => {
 
   describe('checkUniqueArray', () => {
     context('하나의 배열이 주어졌을 때', () => {
-      it('중복이 없는 배열인지 체크한다.', () => {
+      it('중복된 요소가 있는지 체크한다.', () => {
         expect(checkUniqueArray([1, 2])).toBeTruthy();
         expect(checkUniqueArray([1, 1])).toBeFalsy();
       });
